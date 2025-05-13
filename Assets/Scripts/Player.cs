2ec54ad03b2d8c2276b95e1cdcba5cc4894b567a
Waffles24 <jennie.gibb21@gmail.com>
2025-05-13 10:53:06 +0100

    programming
    added jump + left/right movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,19 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 
 
+// Inside the tools menu I changed the line delete tool to ctrl + y!
+
 public class Player : MonoBehaviour
+
 {
-    void start()
-    {
-        
+    private bool jumpKeyWasPressed;
+    private float horizontalInput;
 
-    }
-
-
-    void update()
+    void Start()
     {
 
     }
+
+
+    void Update()
+    //update is called once per frame
+    // anything with a key code/press should be here and not fixed update!
+    //good workflow : check for key press in update, then apply the forces in fixed update
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpKeyWasPressed = true;
+
+        }
+        horizontalInput = Input.GetAxis("Horizontal");
+
+    }
+
+    private void FixedUpdate()
+        //physics stuff here :]
+    {
+        if (jumpKeyWasPressed)
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.up * 5, ForceMode.VelocityChange);
+            jumpKeyWasPressed = false;
+        }
+
+        GetComponent<Rigidbody>().linearVelocity = new Vector3(horizontalInput, GetComponent<Rigidbody>().linearVelocity.y, 0);
+    }
+
 
 }
